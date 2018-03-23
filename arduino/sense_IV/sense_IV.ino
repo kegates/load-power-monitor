5de50fd38e3c48ded3c438f9698934827a5ea407
Kevin Gates <kxg2429@>
2018-03-23 15:04:40 -0400

    changed arduino file to wait for signal from master before sending

--- arduino/sense_IV/sense_IV.ino
+++ arduino/sense_IV/sense_IV.ino
@@ -13,16 +13,16 @@ void setup()
   
   emon1.voltage(2, 120, 0);  // Voltage: input pin, calibration, phase_shift
   emon1.current(1, 60);       // Current: input pin, calibration.
+
+  Serial.println("Send 'D' to recieve real time values.");
 }
 
 void loop()
 {
-  emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
-  emon1.serialprint();           // Print out all variables (realpower, apparent power, Vrms, Irms, power factor)
-  
-  float realPower       = emon1.realPower;        //extract Real Power into variable
-  float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
-  float powerFActor     = emon1.powerFactor;      //extract Power Factor into Variable
-  float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
-  float Irms            = emon1.Irms;             //extract Irms into Variable
+  while(!Serial.available()){}
+  char read = Serial.read();
+  if(read == 'D'){
+    emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
+    emon1.serialprint(); // Print out all variables (realpower, apparent power, Vrms, Irms, power factor)
+  }
 }
